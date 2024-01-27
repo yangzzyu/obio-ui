d3cd18d12085339e3403d01f276f7447fbac5e75
yangyu <1431330771@qq.com>
2024-01-27 15:37:24 +0800

    修改

--- src/views/contact/Contact.vue
+++ src/views/contact/Contact.vue
@@ -2,7 +2,7 @@
  * @Author: yangyu 1431330771@qq.com
  * @Date: 2024-01-18 08:59:26
  * @LastEditors: yangyu 1431330771@qq.com
- * @LastEditTime: 2024-01-27 15:20:11
+ * @LastEditTime: 2024-01-27 15:36:03
  * @FilePath: \obio-ui\src\views\AboutView.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -29,7 +29,6 @@
             </div>
           </div>
           <div class="in-contact-form">
-            <el-row>
               <el-col :span="24" :xs="24">
                 <el-form
                   ref="ruleFormRef"
@@ -163,7 +162,6 @@
                   </div>
                 </el-form>
               </el-col>
-            </el-row>
           </div>
         </el-col>
       </div>
--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -2,7 +2,7 @@
  * @Author: yangyu 1431330771@qq.com
  * @Date: 2024-01-17 09:07:47
  * @LastEditors: yangyu 1431330771@qq.com
- * @LastEditTime: 2024-01-27 14:36:15
+ * @LastEditTime: 2024-01-27 15:28:50
  * @FilePath: \obio-ui\src\views\HomeView.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -518,7 +518,6 @@ function handleInsightsType(params) {
 }
 const ruleForm = ref<FormItem>({});
 const rules = ref([]);
-const scrollTop = ref(0);
 onMounted(() => {
   var wow = new WOW({
     boxClass: "wow",
