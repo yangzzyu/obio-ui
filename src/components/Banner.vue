f90c0f3ce7bcbfdc8309f373d5b3f47a6f359662
yangyu <1431330771@qq.com>
2024-02-04 16:25:25 +0800

    修改

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,7 +2,7 @@
  * @Author: yangyu 1431330771@qq.com
  * @Date: 2024-01-17 11:39:49
  * @LastEditors: yangyu 1431330771@qq.com
- * @LastEditTime: 2024-01-26 15:50:40
+ * @LastEditTime: 2024-02-04 13:42:28
  * @FilePath: \obio-ui\src\components\Banner.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -13,7 +13,7 @@
       <div class="pub-ico rightArrow" @click="arrowClick('right')"></div>
       <el-carousel
         ref="cardShow"
-        :interval="10000"
+        :interval="5000"
         arrow="never"
         :height="bannerHeight + 'px'"
         :autoplay="true"
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
  * @Author: yangyu 1431330771@qq.com
  * @Date: 2024-01-22 21:59:54
  * @LastEditors: yangyu 1431330771@qq.com
- * @LastEditTime: 2024-02-04 09:11:36
+ * @LastEditTime: 2024-02-04 13:44:37
  * @FilePath: \obio-ui\src\components\Header.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -413,14 +413,14 @@ onBeforeMount(() => {});
     color: #000 !important;
   }
 }
-.menu-t1 {
-  font-size: 1.1vw;
-}
-.menu-drawer {
-  .menu-t1 {
-    font-size: 18px;
-  }
-}
+// .menu-t1 {
+//   font-size: 1.1vw;
+// }
+// .menu-drawer {
+//   .menu-t1 {
+//     font-size: 18px;
+//   }
+// }
 </style>
 <style lang="scss" scoped>
 $nav_active_color: #3370ff;
