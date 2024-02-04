43fc083796d79aac3a9a734f9a65776444f3e1fc
yangyu <1431330771@qq.com>
2024-02-04 08:57:22 +0800

    修改

--- src/views/insights/Detail.vue
+++ src/views/insights/Detail.vue
@@ -2,7 +2,7 @@
  * @Author: yangyu 1431330771@qq.com
  * @Date: 2024-01-18 08:59:26
  * @LastEditors: yangyu 1431330771@qq.com
- * @LastEditTime: 2024-01-27 15:42:39
+ * @LastEditTime: 2024-01-27 15:43:48
  * @FilePath: \obio-ui\src\views\AboutView.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -61,7 +61,6 @@ const FocusData = ref({
   visibleXsImgUrl:
     "https://www.obio-tech.com/public/uploads/20220401/fed2a39854c2e2e0bd6d7d8434597de8.jpg",
 });
-const ReleasesList = ref([]);
 const newsItem = ref({});
 onMounted(() => {
   // const currentRoute = router.currentRoute.value;
